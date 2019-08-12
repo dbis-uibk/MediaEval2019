f2f5024da62c5195d604d489460ed4a3b00dbaee
Maximilian Mayerl <maximilian.mayerl@student.uibk.ac.at>
2019-08-12 11:12:57 +0200

    SVM with Essentia features.

---
+++ src/configs/baseline_svm_essentia.py
@@ -0,0 +1,29 @@
+import common
+
+from loaders.acousticbrainz import AcousticBrainzLoader
+
+from dbispipeline.evaluators import FixedSplitGridEvaluator
+import dbispipeline.result_handlers as result_handlers
+
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.multioutput import MultiOutputClassifier
+
+dataloader = AcousticBrainzLoader(
+    training_path="/storage/nas3/datasets/music/mediaeval2019/accousticbrainz-train.pickle",
+    test_path="/storage/nas3/datasets/music/mediaeval2019/accousticbrainz-test.pickle",
+    validation_path="/storage/nas3/datasets/music/mediaeval2019/accousticbrainz-validation.pickle"
+)
+
+pipeline = Pipeline([("scaler", StandardScaler()),
+                     ("model", MultiOutputClassifier(SVC(probability=True)))])
+
+evaluator = FixedSplitGridEvaluator(
+    params={"model__estimator__C": [0.1, 1.0, 10.0]},
+    grid_params=common.grid_params(),
+)
+
+result_handlers = [
+    result_handlers.print_gridsearch_results,
+]
