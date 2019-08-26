d99a0be5eb0ccd6bd47a69d22df544c0b1a8b83c
Michael Vötter <mikevo-uibk@famv.net>
2019-08-26 13:35:16 +0200

    Fix typo

--- src/configs/approach_crnn_label.py
+++ src/configs/approach_crnn_label.py
@@ -7,7 +7,7 @@ from models.crnn import CRNNModel
 from sklearn.metrics import average_precision_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import make_scorer
-from sklearn.metrics import multilabel_confusio_matrix
+from sklearn.metrics import multilabel_confusion_matrix
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import roc_auc_score
@@ -30,30 +30,31 @@ pipeline = Pipeline([
     ("model", CRNNModel(epochs=2, dataloader=dataloader)),
 ])
 
-evaluator = FixedSplitEvaluator(scoring={
-    'f1_micro':
-        make_scorer(f1_score, average='micro'),
-    'f1_macro':
-        make_scorer(f1_score, average='macro'),
-    'roc_auc':
-        make_scorer(roc_auc_score, average='macro'),
-    'average_precision':
-        make_scorer(average_precision_score, average='macro'),
-    'precision_micro':
-        make_scorer(precision_score, average='micro'),
-    'precision_macro':
-        make_scorer(precision_score, average='macro'),
-    'recall_micro':
-        make_scorer(recall_score, average='micro'),
-    'recall_macro':
-        make_scorer(recall_score, average='macro'),
-    'average_precision_all':
-        make_scorer(average_precision_score, average=None),
-    'roc_auc_all':
-        make_scorer(roc_auc_score, average=None),
-    'confusion_matrix':
-        make_scorer(multilabel_confusio_matrix, average=None),
-})
+evaluator = FixedSplitEvaluator(
+    scoring={
+        'f1_micro':
+            make_scorer(f1_score, average='micro'),
+        'f1_macro':
+            make_scorer(f1_score, average='macro'),
+        'roc_auc':
+            make_scorer(roc_auc_score, average='macro'),
+        'average_precision':
+            make_scorer(average_precision_score, average='macro'),
+        'precision_micro':
+            make_scorer(precision_score, average='micro'),
+        'precision_macro':
+            make_scorer(precision_score, average='macro'),
+        'recall_micro':
+            make_scorer(recall_score, average='micro'),
+        'recall_macro':
+            make_scorer(recall_score, average='macro'),
+        'average_precision_all':
+            make_scorer(average_precision_score, average=None),
+        'roc_auc_all':
+            make_scorer(roc_auc_score, average=None),
+        'confusion_matrix':
+            make_scorer(multilabel_confusion_matrix, average=None),
+    })
 
 result_handlers = [
     result_handlers.print_gridsearch_results,
