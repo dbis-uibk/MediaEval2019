ba621cc11ae3098b99ca54f85fbc1536dec65861
Michael Vötter <mikevo-uibk@famv.net>
2019-08-29 09:48:21 +0200

    Refactor code for fixed split evaluation

--- src/common.py
+++ src/common.py
@@ -1,5 +1,10 @@
-from sklearn.metrics import (average_precision_score, f1_score, make_scorer,
-                             precision_score, recall_score, roc_auc_score)
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import make_scorer
+from sklearn.metrics import multilabel_confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import roc_auc_score
 
 DEFAULT_PATH = '/storage/nas3/datasets/music/mediaeval2019'
 
@@ -28,4 +33,33 @@ def grid_params():
         'n_jobs': -1,
         'iid': True,
         'refit': False,
+    }
+
+
+def fixed_split_params():
+    return {
+        'scoring': {
+            'f1_micro':
+                make_scorer(f1_score, average='micro'),
+            'f1_macro':
+                make_scorer(f1_score, average='macro'),
+            'roc_auc':
+                make_scorer(roc_auc_score, average='macro'),
+            'average_precision':
+                make_scorer(average_precision_score, average='macro'),
+            'precision_micro':
+                make_scorer(precision_score, average='micro'),
+            'precision_macro':
+                make_scorer(precision_score, average='macro'),
+            'recall_micro':
+                make_scorer(recall_score, average='micro'),
+            'recall_macro':
+                make_scorer(recall_score, average='macro'),
+            'average_precision_all':
+                make_scorer(average_precision_score, average=None),
+            'roc_auc_all':
+                make_scorer(roc_auc_score, average=None),
+            'confusion_matrix':
+                make_scorer(multilabel_confusion_matrix),
+        },
     }
--- src/configs/baseline_knn_label.py
+++ src/configs/baseline_knn_label.py
@@ -4,16 +4,8 @@ import dbispipeline.result_handlers as result_handlers
 from dbispipeline.utils import prefix_path
 from loaders.librosa_features import LibRosaLoader
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import average_precision_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import make_scorer
-from sklearn.metrics import multilabel_confusion_matrix
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import roc_auc_score
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-
 
 dataloader = LibRosaLoader(
     prefix_path("autotagging_moodtheme-train-librosa.pickle",
@@ -23,33 +15,9 @@ dataloader = LibRosaLoader(
 )
 
 pipeline = Pipeline([("scaler", StandardScaler()),
-                     ("model", KNeighborsClassifier(n_neighbors=10))])
+                     ("model", KNeighborsClassifier(n_neighbors=50))])
 
-evaluator = FixedSplitEvaluator(
-    scoring={
-        'f1_micro':
-            make_scorer(f1_score, average='micro'),
-        'f1_macro':
-            make_scorer(f1_score, average='macro'),
-        'roc_auc':
-            make_scorer(roc_auc_score, average='macro'),
-        'average_precision':
-            make_scorer(average_precision_score, average='macro'),
-        'precision_micro':
-            make_scorer(precision_score, average='micro'),
-        'precision_macro':
-            make_scorer(precision_score, average='macro'),
-        'recall_micro':
-            make_scorer(recall_score, average='micro'),
-        'recall_macro':
-            make_scorer(recall_score, average='macro'),
-        'average_precision_all':
-            make_scorer(average_precision_score, average=None),
-        'roc_auc_all':
-            make_scorer(roc_auc_score, average=None),
-        'confusion_matrix':
-            make_scorer(multilabel_confusion_matrix),
-    })
+evaluator = FixedSplitEvaluator(**common.fixed_split_params())
 
 result_handlers = [
     result_handlers.print_gridsearch_results,
