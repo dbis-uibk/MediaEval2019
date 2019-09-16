9df22f55da2513e1e72d505593493be22642a14e
Michael Vötter <mikevo-uibk@famv.net>
2019-09-16 09:53:49 +0200

    Add more crnn configs

---
+++ src/configs/crnn_32.py
@@ -0,0 +1,34 @@
+import common
+from dbispipeline.evaluators import FixedSplitEvaluator
+from dbispipeline.evaluators import ModelCallbackWrapper
+import dbispipeline.result_handlers as result_handlers
+from dbispipeline.utils import prefix_path
+from loaders.melspectrograms import MelSpectrogramsLoader
+from models.crnn import CRNNModel
+from sklearn.pipeline import Pipeline
+
+WINDOW_SIZE = 1366
+
+dataloader = MelSpectrogramsLoader(
+    data_path=prefix_path("melspec_data", common.DEFAULT_PATH),
+    training_path=prefix_path("autotagging_moodtheme-train.tsv",
+                              common.DEFAULT_PATH),
+    test_path=prefix_path("autotagging_moodtheme-test.tsv",
+                          common.DEFAULT_PATH),
+    validate_path=prefix_path("autotagging_moodtheme-validation.tsv",
+                              common.DEFAULT_PATH),
+    window_size=WINDOW_SIZE,
+)
+
+pipeline = Pipeline([
+    ("model", CRNNModel(epochs=32, dataloader=dataloader)),
+])
+
+evaluator = ModelCallbackWrapper(
+    FixedSplitEvaluator(**common.fixed_split_params()),
+    lambda model: common.store_prediction(model, dataloader),
+)
+
+result_handlers = [
+    result_handlers.print_gridsearch_results,
+]
